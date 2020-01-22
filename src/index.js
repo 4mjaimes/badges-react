db77631be497bb1892ba80dee85b94d1c1582d8b
Manuel Jaimes <manuel.jaimes>
2020-01-22 12:05:32 -0500

    13 - fix: first page

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import BadgeNew from "./pages/BadgeNew";
 import "bootstrap/dist/css/bootstrap.css";
 import "./assets/styles/global.css";
 
-// import Badge from "./components/Badge";
-import BadgeNew from "./pages/BadgeNew";
 
 const container = document.getElementById("App");
 
