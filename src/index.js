39e6484695e26f1d40a39295005bd386c2de9b77
Manuel Jaimes <4mjaimes@Manuels-Mac-mini.local>
2020-01-21 21:30:26 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const element = <h1>Hello, from React Badges!.</h1>;
+const container = document.getElementById('App');
+
+ReactDOM.render(element, container);
